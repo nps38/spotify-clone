8f9ba8b765e044626163a1ee75849a06ffe19f34
Neha Shirwalker <nps38@njit.edu>
2023-07-12 19:02:23 -0400

    get token from URL

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,13 +1,41 @@
-import './App.css';
-import React from "react"
+import './App.css'
+import React, { useEffect, useState } from "react"
 import Login from "./Login"
+import { getTokenFromUrl } from './spotify'
+import SpotifyWebApi from "spotify-web-api-js"
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+const [token, setToken] = useState(null);
+
+  //run code based on given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token){
+      setToken(_token)
+
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user => {
+        console.log('♥', user);
+      })
+    }
+  }, []);
+
   return (
     <div className="app">
       {/* Spotify logo */}
       {/* Login with spotify button */}
-      <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
