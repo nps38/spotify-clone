e6104e5175df0776d2c58b4b913a9a85b87499f1
Neha Shirwalker <nps38@njit.edu>
2023-08-15 19:02:52 -0400

    style sidebar and install mui icons

--- spotify-clone-app/src/SidebarOption.js
+++ spotify-clone-app/src/SidebarOption.js
@@ -4,7 +4,8 @@ import "./SidebarOption.css"
 function SidebarOption({ title, Icon }) {
   return (
     <div className="sidebarOption">
-        <p>{title}</p>
+        {Icon && <Icon className="sidebarOption_icon" />}
+        {Icon ? <h4>{title}</h4> : <p>{title}</p>}
     </div>
   )
 }
