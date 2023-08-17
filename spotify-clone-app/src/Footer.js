6b6576e18975292e6e138ac86694cb5a3d90dfa1
Neha Shirwalker <nps38@njit.edu>
2023-08-17 15:42:40 -0400

    import footer icons

--- spotify-clone-app/src/Footer.js
+++ spotify-clone-app/src/Footer.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import "./Footer.css";
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
 
 function Footer() {
   return (
