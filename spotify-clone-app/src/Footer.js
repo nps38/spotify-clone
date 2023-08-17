a86b9fc2f98ef0f0ac2959cfda1acaa6e8212f18
Neha Shirwalker <nps38@njit.edu>
2023-08-17 15:36:59 -0400

    basic footer set up

--- spotify-clone-app/src/Footer.js
+++ spotify-clone-app/src/Footer.js
@@ -3,7 +3,17 @@ import "./Footer.css"
 
 function Footer() {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">
+      <div className="footer_left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer_center">
+        <p>Player controls</p>
+      </div>
+      <div className="footer_right">
+        <p>volume controls</p>
+      </div>
+    </div>
   )
 }
 
