35f84e95b50dda707d50858f7506ae65208e82f9
Neha Shirwalker <nps38@njit.edu>
2023-08-17 18:43:06 -0400

    pull user profile and name from account

--- spotify-clone-app/src/Header.js
+++ spotify-clone-app/src/Header.js
@@ -2,24 +2,27 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search'
 import { Avatar } from '@mui/material'
+import { useDataLayerValue } from './DataLayer'
 
 function Header() {
-  return (
-    <div className="header">
-        <div className="header_left">
-            <SearchIcon />
-            <input
-                placeholder="Search for Artists, Songs"
-                type="text"
-            />
-        </div>
+    const [{ user }, dispatch] = useDataLayerValue();
 
-        <div className="header_right">
-            <Avatar src="" alt="RQ" />
-            <h4>Name</h4>
+    return (
+        <div className="header">
+            <div className="header_left">
+                <SearchIcon />
+                <input
+                    placeholder="Search for Artists, Songs"
+                    type="text"
+                />
+            </div>
+
+            <div className="header_right">
+                <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+                <h4>{user?.display_name}</h4>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
