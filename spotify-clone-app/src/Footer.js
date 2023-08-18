1a0fb0d3f653a71d36eae270e41ad2d6b82580aa
Neha Shirwalker <nps38@njit.edu>
2023-08-17 22:17:28 -0400

    footer icons functionality

--- spotify-clone-app/src/Footer.js
+++ spotify-clone-app/src/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Footer.css";
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
@@ -8,23 +8,112 @@ import RepeatIcon from '@mui/icons-material/Repeat';
 import { Grid, Slider } from '@mui/material';
 import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
 import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import { useDataLayerValue } from './DataLayer';
 
-function Footer() {
+function Footer({ spotify }) {
+  const [{ token, item, playing }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    spotify.getMyCurrentPlaybackState().then((r) => {
+      console.log(r);
+
+      dispatch({
+        type: "SET_PLAYING",
+        playing: r.is_playing,
+      });
+
+      dispatch({
+        type: "SET_ITEM",
+        item: r.item,
+      });
+    });
+  }, [spotify]);
+
+  const handlePlayPause = () => {
+    if (playing) {
+      spotify.pause();
+      dispatch({
+        type: "SET_PLAYING",
+        playing: false,
+      });
+    } else {
+      spotify.play();
+      dispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    }
+  };
+
+  const skipNext = () => {
+    spotify.skipToNext();
+    spotify.getMyCurrentPlayingTrack().then((r) => {
+      dispatch({
+        type: "SET_ITEM",
+        item: r.item,
+      });
+      dispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    });
+  };
+
+  const skipPrevious = () => {
+    spotify.skipToPrevious();
+    spotify.getMyCurrentPlayingTrack().then((r) => {
+      dispatch({
+        type: "SET_ITEM",
+        item: r.item,
+      });
+      dispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    });
+  };
+
   return (
     <div className="footer">
       <div className="footer_left">
-        <img className="footer_albumLogo" src="" alt="" />
-        <div className="footer_songInfo">
-          <h4>Warm Glow</h4>
-          <p>Hippo Campus</p>
-        </div>
+        <img 
+          className="footer_albumLogo" 
+          src={item?.album.images[0].url}
+          alt={item?.name}
+        />
+        {item ? (
+          <div className="footer_songInfo">
+            <h4>{item.name}</h4>
+            <p>{item.artists.map((artist) => artist.name).join(", ")}</p>
+          </div>
+        ) : (
+          <div className="footer_songInfo">
+            <h4>No song is playing</h4>
+            <p>...</p>
+          </div>
+        )}
       </div>
+      
 
       <div className="footer_center">
         <ShuffleIcon className="footer_green" />
-        <SkipPreviousIcon className="footer_icon" />
-        <PlayCircleOutlineIcon fontSize="large" className="footer_icon"/>
-        <SkipNextIcon className="footer_icon" />
+        <SkipPreviousIcon onClick={skipNext} className="footer_icon" />
+        
+        {playing ? (
+          <PlayCircleOutlineIcon
+            onClick={handlePlayPause}
+            fontSize="large" 
+            className="footer_icon"
+          />
+        ) : (
+          <PlayCircleOutlineIcon
+            onClick={handlePlayPause}
+            fontSize="large"
+            className="footer_icon"
+          />
+        )}
+        
+        <SkipNextIcon onClick={skipPrevious} className="footer_icon" />
         <RepeatIcon className="footer_green" />
       </div>
 
